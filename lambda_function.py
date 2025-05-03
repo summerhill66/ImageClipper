d1b911b73f2007c349cfaf4f922c79f65c6f940a
summerhill66 <summerhill66rd@gmail.com>
2025-05-03 18:42:11 +0900

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,24 @@
+import json
+
+def lambda_handler(event, context):
+    html_content = """
+    <html>
+        <head>
+            <title>Lambda Web Page</title>
+        </head>
+        <body>
+            <h1>Welcome to my Lambda-powered Web Page!</h1>
+            <p>This page is served by AWS Lambda using Python!</p>
+        </body>
+    </html>
+    """
+
+    response = {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/html',
+        },
+        'body': html_content
+    }
+
+    return response
