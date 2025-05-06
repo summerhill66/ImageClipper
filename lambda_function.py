4c5fac86504ce9bbe1be6f1c6c5ebab1f4f5e6aa
summerhill66 <summerhill66rd@gmail.com>
2025-05-06 13:55:22 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -68,7 +68,6 @@ def handle_delete(event):
         return {
             "statusCode": 200, 
             "headers": {
-                "Location": "/", 
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Methods": "POST,OPTIONS"
