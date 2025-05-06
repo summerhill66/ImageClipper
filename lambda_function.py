3ea98117fafd23d24afcb4497082290ca5808d4d
summerhill66 <summerhill66rd@gmail.com>
2025-05-06 22:58:52 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -111,7 +111,7 @@ def render_gallery():
             .image-container {{ display: inline-block; margin: 10px; position: relative; }}
             img {{ width: 150px; height: 150px; object-fit: cover; cursor: pointer; border: 1px solid #ccc; border-radius: 8px; }}
             .modal {{ display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.8); }}
-            .modal-content {{ margin: 5% auto; display: block; max-width: 90vm; max-height: 90vh; border-radius: 8px; box-shadow: 0 0 10px #fff;}}
+            .modal-content {{ margin: 5% auto; display: block; max-width: 90vw; max-height: 90vh; border-radius: 8px; box-shadow: 0 0 10px #fff;}}
         </style>
         <script>
             function toggleDeleteMode() {{
